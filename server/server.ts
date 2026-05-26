e9b6075ecfd2afe58e4b5d886e52fec484fe6123
TmDevelopment <tmgamig69@gmail.com>
2026-05-26 18:17:56 +0530

    Update server.ts

--- server/server.ts
+++ server/server.ts
@@ -7,6 +7,9 @@ import uploadRouter from "./routes/uploadRotues.js";
 import orderRouter from "./routes/orderRoutes.js";
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
+import addressRouter from "./routes/addressRoutes.js";
+import adminRouter from "./routes/adminRoutes.js";
+import deliveryPartnerRouter from "./routes/deliveryPartnerRoutes.js";
 const app = express();
 
 // Middleware
@@ -23,6 +26,9 @@ app.use('/api/products', productRouter);
 app.use('/api/upload', uploadRouter);
 app.use('/api/orders', orderRouter);
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/addresses", addressRouter);
+app.use("/api/admin", adminRouter);
+app.use("/api/delivery", deliveryPartnerRouter);
 
 // Global error handler
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
